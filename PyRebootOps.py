7da2282ebeded4217f66cfa01a16b4e5ae5274e0
Chetan <cjgiridhar@gmail.com>
2012-07-24 14:41:55 +0530

    Pyrebootops1.0

--- PyRebootOps.py
+++ PyRebootOps.py
@@ -15,7 +15,7 @@ _app_name = "PyRebootOps"
 # it under the terms of the GNU General Public License v3 as published by
 # the Free Software Foundation;
 #
-# seFS is distributed in the hope that it will be useful,
+# PyRebootOps is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
